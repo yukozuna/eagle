92a510bb70a236d7ed1639c06397c943c47360dd
yukozuna <yazan.m@gmail.com>
2020-05-29 23:06:06 +1000

    compdir.py takes input of two directories and compares to see if they are identical by comparing each subdirectory

---
+++ compdir.py
@@ -0,0 +1,35 @@
+#this script takes the given paths as a cli argument and checks if they are identical
+
+import sys
+import os
+from filecmp import dircmp
+
+def dir_compare(d1,d2):
+    dcmp = dircmp(d1, d2)
+    if (dcmp.left_list == dcmp.same_files) and (dcmp.right_list == dcmp.same_files):
+        return False if len(dcmp.same_files) == 0 else True #don't need empty directories
+    else:
+        return False
+
+def fast_scandir(dirname):
+    subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
+    for dirname in list(subfolders):
+        subfolders.extend(fast_scandir(dirname))
+    return subfolders
+
+path1 = sys.argv[1]
+path2 = sys.argv[2]
+
+if os.path.isdir(path1) == False:
+    print('Path1 not found')
+    exit()
+
+if os.path.isdir(path2) == False:
+    print('Path2 not found')
+    exit()
+
+dir1 = fast_scandir(path1)
+dir2 = fast_scandir(path2)
+
+print('Found',len(dir1),'subdirectories in path 1' )
+print('Found',len(dir2),'subdirectories in path 2' )
